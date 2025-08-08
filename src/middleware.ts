1d10cc4b9fa2230593d5833ad6100d542817fb4b
kushagra-aa <kushagraagnihotri24@gmail.com>
2025-08-08 21:01:48 +0530

    Code: Protected Frontend Routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,49 @@
-import { checkUserSession } from "@/lib/session";
+import { checkUserSession, deleteUserSession } from "@/lib/session";
 import { sendAPIError } from "@/utils/backendHelpers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export default async function middleware() {
-  const response = NextResponse.next();
-  const user = await checkUserSession();
+const apiMiddleware = async (
+  req: NextRequest,
+  next: NextResponse<unknown>,
+  user?: { email: string }
+) => {
   if (user) {
-    response.headers.set("x-user-email", user.email);
-    return response;
+    next.headers.set("x-user-email", user.email);
+    return next;
   }
+  await deleteUserSession();
   return sendAPIError({
     error: "Unauthorized",
     status: 401,
     message: "User is Unauthorized",
   });
+};
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const next = NextResponse.next();
+  const user = await checkUserSession();
+  const isPublicRoute =
+    path.startsWith("/login") || path.startsWith("/register");
+
+  if (path.startsWith("/api")) return await apiMiddleware(req, next, user);
+
+  if (!user && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+  if (user && isPublicRoute) {
+    return NextResponse.redirect(new URL("/boards", req.url));
+  }
+
+  return next;
 }
 
 export const config = {
-  matcher: "/api/boards/:path*",
+  matcher: [
+    "/login",
+    "/register",
+    "/boards/:path*",
+    "/boards/:path*/tasks",
+    "/api/boards/:path*",
+  ],
 };
